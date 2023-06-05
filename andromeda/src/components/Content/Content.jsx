3159798b704b4bb912fd1de5b809303c77224713
isal-man <faiskweren7@gmail.com>
2023-06-05 19:42:57 +0700

    Add content

--- andromeda/src/components/Content/Content.jsx
+++ andromeda/src/components/Content/Content.jsx
@@ -1,9 +1,9 @@
 const Content = () => {
     const students = [
-        { id: 1, name: "Ahmad Faisal Hidayat" },
-        { id: 2, name: "Muhammad Ikrar Wiratama" },
-        { id: 3, name: "Muhammad Yazid Irham" }
-    ]
+        { id: 1, nim: "001", name: "Ahmad Faisal Hidayat" },
+        { id: 2, nim: "002", name: "Muhammad Ikrar Wiratama" },
+        { id: 3, nim: "003", name: "Muhammad Yazid Irham" },
+    ];
 
     return (
         <>
@@ -11,8 +11,24 @@ const Content = () => {
                 Hello, Universe!
             </h1>
             <ul>
-                {students.map((student) => <li key={student.id}>{student.name}</li>)}
+                {students.map((student) => (
+                    <li key={student.id}>{student.name}</li>
+                ))}
             </ul>
+            <table border={2} cellPadding={5}>
+                <tbody>
+                    <tr>
+                        <th>Nim</th>
+                        <th>Name</th>
+                    </tr>
+                    {students.map((student, i) => (
+                            <tr key={i}>
+                                <td>{student.nim}</td>
+                                <td>{student.name}</td>
+                            </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 };
